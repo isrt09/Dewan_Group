7fe34f85ccc40fbd1babde99ee15069276ef1987
isrt09 <mazedur@gmail.com>
2025-11-26 13:20:10 +0600

    First Class task assigned done

---
+++ AMAD_Class_01/FirstClass/src/Dewan.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var name : String = "Dewan ICT Institute"
+    val year : Int    = 2013
+
+    println("Institute Name is $name")
+    println("Established Year is $year")
+
+    // var can be reassigned. It's a mutable category
+    // val can not reassigned. It's a immutable types
+}
